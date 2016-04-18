25051271a0bec49211b3540ab9aab0aeeb4bc921
Rashedul Islam <ririyad.291@gmail.com>
2016-04-18 19:13:32 +0600

    edited html file

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import todo
 from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 def index(request): #Define our function, accept a request
-
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     items = todo.objects.all() #ORM queries the database for all of the to-do entries.
     return render_to_response('index.html', {'items': items})
