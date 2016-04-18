031294d2b6a73a14f14138a5247ca71c94c6cd7b
Rashedul Islam <ririyad.291@gmail.com>
2016-04-18 19:59:05 +0600

    edited index file

--- core/views.py
+++ core/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 def index(request): #Define our function, accept a request
     if not request.user.is_staff or not request.user.is_superuser:
         raise Http404
-    items = todo.objects.all() #ORM queries the database for all of the to-do entries.
+    items = todo.objects.all() # ORM queries the database for all of the to-do entries.
     return render_to_response('index.html', {'items': items})
